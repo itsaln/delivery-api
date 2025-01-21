17b5432ec7f42f4b2f7da7dcc80a9a316b8e98bd
Aladin Mukhammadiev <itsaln23@gmail.com>
2025-01-21 16:09:53 +0500

    fix, uploads

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
+import { ServeStaticModule } from '@nestjs/serve-static'
+import { path } from 'app-root-path'
 import { AppController } from '@app/app.controller'
 import { AppService } from '@app/app.service'
 import { AuthModule } from '@app/auth/auth.module'
@@ -9,6 +11,10 @@ import { UserModule } from '@app/user/user.module'
 
 @Module({
 	imports: [
+		ServeStaticModule.forRoot({
+			rootPath: `${path}/uploads`,
+			serveRoot: '/uploads'
+		}),
 		ConfigModule.forRoot(),
 		AuthModule,
 		CategoryModule,
